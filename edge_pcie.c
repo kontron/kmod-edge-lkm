585f660682bcac53b1f244333c16f50e578ea1d1
Michael Walle <michael.walle@kontron.com>
2020-11-20 14:02:50 +0100

    update PHY timestampings
    We are using the non low-latency mode for now. So also use the correct
    delays.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- edge_pcie.c
+++ edge_pcie.c
@@ -256,7 +256,7 @@ static int edgx_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 			goto probe_out_link;
 		}
 
-		edgx_link_set_delays(lnk, 1663, 156, 92, 1140, 180, 207);
+		edgx_link_set_delays(lnk, 4432, 452, 122, 1133, 220, 207);
 	}
 
 	tsnic_vpd_asset_tag(pci_drv->vpd, asset, sizeof(asset));
